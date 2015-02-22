2a7b8e96c2d6a45584919169a7b58b2fd15ca97f
Administrator <solinari@ubuntu.ubuntu-domain>
2015-02-22 15:33:02 +0800

    version alpha v0.01

---
+++ src/MainLoader.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author solinari
+ *
+ */
+public class MainLoader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
